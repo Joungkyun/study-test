c529c6c6f626903d2eaae253a25b57e88cc57a7f
JoungKyun Kim <joungkyun@gmail.com>
2019-03-26 21:55:30 +0900

    Update trim_test.c

--- trim_test.c
+++ trim_test.c
@@ -33,3 +33,12 @@ int main (void) {
 
 	return 0
 }
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: ai et sw=4 ts=4 fdm=marker
+ * vim<600: ai et sw=4 ts=4
+ */
